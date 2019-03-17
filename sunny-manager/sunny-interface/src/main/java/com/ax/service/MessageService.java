005f9bac2530a929771e63948a558c3a02fdb603
许晨 <x3408@hotmail.com>
2019-03-17 10:17:32 +0800

    消息功能

--- sunny-manager/sunny-interface/src/main/java/com/ax/service/MessageService.java
+++ sunny-manager/sunny-interface/src/main/java/com/ax/service/MessageService.java
@@ -1,10 +1,15 @@
 package com.ax.service;
 
-/**
+import com.ax.entity.Result;
+import com.ax.pojo.TbMessage;
+import com.ax.pojo.TbUser; /**
  *  消息接口
  * */
 public interface MessageService {
 
+
 //    增加
+    Result add(TbUser user, TbMessage message) throws Exception;
 //    查询
+    Result get(TbUser user);
 }
--- sunny-manager/sunny-manager-web/src/main/java/com/ax/controller/MessageController.java
+++ sunny-manager/sunny-manager-web/src/main/java/com/ax/controller/MessageController.java
@@ -1,10 +1,33 @@
 package com.ax.controller;
 
+import com.ax.entity.Result;
+import com.ax.pojo.TbMessage;
+import com.ax.pojo.TbUser;
+import com.ax.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/message")
 public class MessageController {
+    @Autowired
+    private MessageService messageService;
 
+    @RequestMapping("/add")
+    public Result add(TbMessage message, HttpServletRequest request) {
+        TbUser user = (TbUser) request.getSession().getAttribute("user");
+        try {
+            return messageService.add(user, message);
+        } catch (Exception e) {
+            return new Result(false, "添加对话失败!");
+        }
+    }
+
+    public Result get(HttpServletRequest request) {
+        TbUser user = (TbUser) request.getSession().getAttribute("user");
+        return messageService.get(user);
+    }
 }
